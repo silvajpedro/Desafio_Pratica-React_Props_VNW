b57cb009506d7681d4e362a5df157eca8c30608d
silvajpedro <belojpedro.31@gmail.com>
2022-08-12 20:22:11 -0300

    ajeitando botão

--- src/Components/Style.js
+++ src/Components/Style.js
@@ -56,12 +56,13 @@ export const Caixa = () => {
         <div>
             <Container2>
                 <p>Clique no botão </p>
-                <button onMouseOver={Cor} onClick={Crescer} >Clique aqui</button>
+                <button onClick={Crescer} >Clique aqui</button>
             </Container2>
         </div>
 
-    )
+)
 }
+let troca = 0
 let contagem = 0
 const Crescer = () => {
     if (contagem === 0) {
@@ -69,46 +70,36 @@ const Crescer = () => {
         caixa.style.transform = "scale(100%)"
         caixa.style.transition = "ease-in-out 1s"
         contagem += 1
+        troca += 1
     } else if (contagem === 1) {
         let caixa = document.querySelector(".vermelha")
         caixa.style.transform = "scale(0.1%)"
         caixa.style.transition = "ease-in-out 1s"
         contagem -= 1
+        troca += 1
     }
-}
-
-let troca = 0
-const Cor = () => {
     if (troca === 0) {
         document.querySelector('body').style.backgroundColor = "blue"
-        troca += 1
     } else if (troca === 1) {
          document.querySelector('body').style.backgroundColor = "yellow"
-        troca += 1
     } else if (troca === 2) {
         document.querySelector('body').style.backgroundColor = "purple"
-        troca += 1
     } else if (troca === 3) {
          document.querySelector('body').style.backgroundColor = "orange"
-        troca += 1
     }
     else if (troca === 4) {
          document.querySelector('body').style.backgroundColor = "pink"
-        troca += 1
     }
     else if (troca === 5) {
        document.querySelector('body').style.backgroundColor = "brown"
-        troca += 1
     }
     else if (troca === 6) {
          document.querySelector('body').style.backgroundColor = "grey"
-        troca += 1
     }
     else if (troca === 7) {
         document.querySelector('body').style.backgroundColor = "teal"
-        troca += 1
     } else if (troca === 8) {
          document.querySelector('body').style.backgroundColor = "white"
-        troca -= 8
+         troca-=8
     }
 }
